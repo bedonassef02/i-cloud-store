f82843d557a80e565ce2bcc465857569bd312162
Bedo <bedonassef71@gmail.com>
2023-08-28 01:37:26 +0300

    products crud

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,21 @@
-import {Module, ValidationPipe} from '@nestjs/common';
+import {Module} from '@nestjs/common';
 import {AppController} from './app.controller';
 import {AppService} from './app.service';
 import {CategoriesModule} from './categories/categories.module';
 import {MongooseModule} from "@nestjs/mongoose";
-import {APP_FILTER, APP_PIPE} from "@nestjs/core";
+import {APP_FILTER} from "@nestjs/core";
 import {DuplicateKeyExceptionFilter} from "./common/filters/dublicate-key-exception.filter";
 import {SubCategoriesModule} from './sub-categories/sub-categories.module';
 import {CommonModule} from './common/common.module';
 import {ProductsModule} from './products/products.module';
 import {MulterModule} from "@nestjs/platform-express";
+import {ConfigModule} from "@nestjs/config";
 
 @Module({
-    imports: [MongooseModule.forRoot('mongodb+srv://bedo:bedonassef71@cluster0.pyyzclc.mongodb.net/olx'), MulterModule.register({
-        dest: './upload',
-    }), CategoriesModule, SubCategoriesModule, CommonModule, ProductsModule],
+    imports: [ConfigModule.forRoot({expandVariables: true}),
+        MongooseModule.forRoot(process.env.DB_URI), MulterModule.register({
+            dest: './upload',
+        }), CategoriesModule, SubCategoriesModule, CommonModule, ProductsModule],
     controllers: [AppController],
     providers: [AppService,
         {
