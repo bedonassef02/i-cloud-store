58c52f18d3eba5e0a2b6cdac09006006fdd77d88
Bedo <bedonassef71@gmail.com>
2023-09-04 00:52:25 +0300

    enhance

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,19 @@ import { ValidationPipe } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
+
   const app = await NestFactory.create(AppModule);
+
   app.setGlobalPrefix('api/v1');
+
   app.useGlobalPipes(
     new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
   );
+
   app.use(cookieParser());
-  await app.listen(3000);
+
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
 }
 
 bootstrap();
